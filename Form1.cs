8f2c756395593ce812948eaf5a6a7cb4413f429c
Edgar Ale <edgargonzalezalejandro0@gmail.com>
2023-04-24 18:22:16 -0500

    first part of the project

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //for some reason, recognizes 'setButton' as 'button3'
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+#PROYECT IMAGE VIEWER
+
+This project can be found in Microsoft's official documentation, [here](https://learn.microsoft.com/es-es/visualstudio/get-started/csharp/tutorial-windows-forms-picture-viewer-layout?view=vs-2022)
+
+In this first part the buttons do not have functionality yet. 
